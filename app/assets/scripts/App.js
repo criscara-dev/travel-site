ea633ee3b5257568d05f6799be8195d16f6e897c
Cris <c.caratti@me.com>
2017-08-29 21:30:10 +0100

    working on mobile menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,3 @@
+import MobileMenu from './modules/MobileMenu';
 
-var $ = require('jquery');
-//var Person = require('./modules/Person'); requie is a Node method
-import Person from './modules/Person';
-
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes $0 in taxes. ");
-    }
-}
-
-alert('ABC 645');
-
-var john = new Person('John Doe', 'blue');
-john.greet();
-
-var jane = new Adult('Jane Doe', 'orangeYellow');
-jane.greet();
-jane.payTaxes();
-
-$('h1').remove();
+var mobileMenu = new MobileMenu();
